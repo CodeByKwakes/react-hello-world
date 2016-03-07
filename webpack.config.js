5e0f07e7f89f02d127d8e6da2ac5afabf0873465
KwakesProject <kwakes@me.com>
2016-03-07 14:07:01 +0000

    second way to write the webpack.config setup

--- webpack.config.js
+++ webpack.config.js
@@ -21,3 +21,29 @@ module.exports = {
     ]
   }
 }
+
+/*var config = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 7777
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;*/
